cf603889dd2988d435526fc2a4d71216f79b6164
AlexZ33 <zhaokang1@xiaomi.com>
2020-12-03 17:17:49 +0800

    [新增]通过ｃ语言了解ｇｃｃ编译器如何工作的

---
+++ C/intro/hello.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: AlexZ33
+ * @Date: 2020-12-03
+ * @LastEditTime: 2020-12-03 16:48:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /C/intro/hello.c
+ */
+#include <stdio.h>
+int main()
+{
+	printf("hello, world\n");
+	return 0;
+}
